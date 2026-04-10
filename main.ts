047ed2f62805d64f107f86b1f4c7ba3f94671e74
jpman-school-account <jakub.petrovic@zak.gym-rce.cz>
2026-04-10 17:52:19 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,50 @@
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . # # # .
-    . # # # .
-    # # # # #
-    `)
-basic.pause(200)
-basic.showLeds(`
-    . . # . .
-    . # # # .
-    # # # # #
-    # # # # #
-    # . . . #
-    `)
-basic.pause(200)
-basic.showLeds(`
-    . # # # .
-    # # # # #
-    # # # # #
-    # . . . #
-    . . . . .
-    `)
-basic.pause(200)
-basic.showLeds(`
-    . # # # .
-    # # # # #
-    # . . . #
-    . . . . .
-    . . . . .
-    `)
-basic.pause(200)
-basic.showLeds(`
-    . # # # .
-    # . . . #
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.pause(200)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        # # # # #
+        # . . . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # . . . #
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
 })
